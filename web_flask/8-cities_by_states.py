bf922fc997222bc4a7e80f12e0e66c177c3f9ba5
Ahmed0548 <ahmedalkasape@gmail.com>
2023-05-19 07:30:11 -0700

    Task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-
+"""Flask app to generate html list of all states from storage"""
 from flask import Flask, render_template
-import models
+from models import storage
+app = Flask('web_flask')
+app.url_map.strict_slashes = False
 
-app = Flask("__name__")
+
+@app.route('/cities_by_states')
+def list_of_states():
+    """Render as html alphabetical list of states and cities in each state
+    from `storage`"""
+    states = storage.all('State')
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def refresh(exception):
-        models.storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
-def route_states():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('7-states_list.html', states_list=states)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def route_city():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('8-cities_by_states.html', states_list=states)
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
+    storage.close()
 
 
 if __name__ == "__main__":
-        app.run()
+    app.run(host='0.0.0.0', port=5000)
